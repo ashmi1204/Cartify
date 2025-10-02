c5dbfa038e59e2dc7fccb83cac466923bd51e827
ashmi1204 <ashmiprakash12@gmail.com>
2025-10-02 20:16:47 +0530

    product model and product repo updated..

--- demo/src/main/java/com/example/demo/model/Product.java
+++ demo/src/main/java/com/example/demo/model/Product.java
@@ -1,4 +1,81 @@
 package com.example.demo.model;
 
 public class Product {
+
+    private Long id;
+    private String name;
+    private String description;
+    private double price;
+    private String category;
+    private String image;
+    private int quantity;
+
+    public Product() {
+    }
+
+    public Product(Long id, String name, String description, double price, int quantity, String category, String image) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+        this.category = category;
+        this.image = image;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
 }
--- demo/src/main/java/com/example/demo/repository/productRepo.java
+++ demo/src/main/java/com/example/demo/repository/productRepo.java
@@ -1,4 +1,80 @@
 package com.example.demo.repository;
 
+import com.example.demo.model.Product;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public class productRepo {
+    private JdbcTemplate jdbcTemplate;
+    private final RowMapper<Product> productRowMapper = new RowMapper<Product>() {
+        @Override
+        public Product mapRow(ResultSet rs, int rowNum) throws SQLException {
+            Product product = new Product();
+            product.setId(rs.getLong("product_id"));
+            product.setName(rs.getString("product_name"));
+            product.setDescription(rs.getString("product_description"));
+            product.setCategory(rs.getString("category"));
+            product.setImage(rs.getString("image"));
+            product.setPrice(rs.getDouble("product_price"));
+            product.setQuantity(rs.getInt("product_qty"));
+            return product;
+        }
+    };
+
+    public productRepo(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    //CRUD
+
+    public int save(Product product){
+        String sql = "INSERT INTO products(product_name,product_description,product_price,product_qty,category,image) VALUES(?,?,?,?,?,?)";
+        return jdbcTemplate.update(sql,
+                product.getName(),
+                product.getDescription(),
+                product.getPrice(),
+                product.getQuantity(),
+                product.getCategory(),
+                product.getImage()
+                );
+    }
+
+    public Optional<Product> findById(Long id){
+        String sql = "SELECT product_id, product_name, product_description, product_price, product_qty, category, image FROM Products WHERE product_id = ?";
+        try{
+            Product product = jdbcTemplate.queryForObject(sql,productRowMapper,id);
+            return Optional.ofNullable(product);
+        }
+        catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+    }
+    public int update(Product product) {
+        String sql = "UPDATE Products SET product_name = ?, product_description = ?, product_price = ?, product_qty = ?, category = ?, image = ? WHERE product_id = ?";
+
+        return jdbcTemplate.update(
+                sql,
+                product.getName(),
+                product.getDescription(),
+                product.getPrice(),
+                product.getQuantity(),
+                product.getCategory(),
+                product.getImage(),
+                product.getId()
+        );
+    }
+
+    public int deleteById(Long id) {
+        String sql = "DELETE FROM Products WHERE product_id = ?";
+
+        return jdbcTemplate.update(sql, id);
+    }
 }
